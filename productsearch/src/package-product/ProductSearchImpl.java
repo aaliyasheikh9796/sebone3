2df3c02ac03d888935b39610eb24817125dd8b7a
aaliyasheikh9796 <101250776+aaliyasheikh9796@users.noreply.github.com>
2022-03-24 21:53:11 +0530

    Add files via upload

---
+++ productsearch/src/package-product/ProductSearchImpl.java
@@ -0,0 +1,16 @@
+/**@ClassName-ProductData
+ *@Method-
+ *@Objective-to implement the methods of interface
+ *@param-string,int
+ *@ReturnType-productList
+ *
+ */
+package com.sebone.java.product.implementation;
+
+/**
+ * @author Aaliya Sheikh
+ *
+ */
+public class ProductSearchImpl {
+
+}
