21084710adddb51a659da7e589026610c03d6ea3
aaliyasheikh9796 <101250776+aaliyasheikh9796@users.noreply.github.com>
2022-03-24 21:58:58 +0530

    Add files via upload

---
+++ productsearch/src/package-productsearch/ProductDiscount.java
@@ -0,0 +1,61 @@
+/**@ClassName-ProductDiscount
+ *@Method-All the getter and setter for the constraints
+ *@Objective-To set and get the value
+ *@param-string,integer
+ *@ReturnType-int ,string
+ *
+ * 
+ */
+package com.sebone.java.searchProduct.data;
+
+/**
+ * @author Aaliya Sheikh
+ *
+ */
+public class ProductDiscount {
+	private int productDiscount;
+	private int productDiscountPercent;
+	private String productDiscountStatus;
+	
+	/**
+	 * @return the productDiscount
+	 */
+	public int getProductDiscount() {
+		return productDiscount;
+	}
+	/**
+	 * @param productDiscount the productDiscount to set
+	 */
+	public void setProductDiscount(int productDiscount) {
+		this.productDiscount = productDiscount;
+	}
+	/**
+	 * @return the productDiscountPercent
+	 */
+	public int getProductDiscountPercent() {
+		return productDiscountPercent;
+	}
+	/**
+	 * @param productDiscountPercent the productDiscountPercent to set
+	 */
+	public void setProductDiscountPercent(int productDiscountPercent) {
+		this.productDiscountPercent = productDiscountPercent;
+	}
+	/**
+	 * @return the productDiscountStatus
+	 */
+	public String getProductDiscountStatus() {
+		return productDiscountStatus;
+	}
+	/**
+	 * @param productDiscountStatus the productDiscountStatus to set
+	 */
+	public void setProductDiscountStatus(String productDiscountStatus) {
+		this.productDiscountStatus = productDiscountStatus;
+	}
+	/**
+	 * @return the productDiscountStartTime
+	 */
+	
+	
+}
