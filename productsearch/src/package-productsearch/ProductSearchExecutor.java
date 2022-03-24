2b8242740b1e09151dd2dcc712db8ea5cd873fc5
aaliyasheikh9796 <101250776+aaliyasheikh9796@users.noreply.github.com>
2022-03-24 21:57:59 +0530

    Add files via upload

---
+++ productsearch/src/package-productsearch/ProductSearchExecutor.java
@@ -0,0 +1,26 @@
+/**@ClassName-ProductSearchExecutor
+ *@Method-All the getter and setter for the List of data class
+ *@Objective-To set and get the value
+ *@param-will return List of product by sorting
+ *@ReturnType-List
+ *
+ * 
+ */
+package com.sebone.java.searchProduct.data;
+
+import java.util.ArrayList;
+
+/**
+ * @author Aaliya Sheikh
+ *
+ */
+public class ProductSearchExecutor {
+	ArrayList<ProductData> productResultList=new ArrayList<ProductData>();
+	public ArrayList<ProductData>getproductResultList(){
+		return productResultList;
+		
+	}
+	ArrayList<ProductSearchExecutor> productAttributesList=new ArrayList<ProductSearchExecutor>();
+
+
+}
