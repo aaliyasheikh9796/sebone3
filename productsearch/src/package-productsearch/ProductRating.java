ba32ddf8eedb32bf7246d8f866cc4f47a168431f
aaliyasheikh9796 <101250776+aaliyasheikh9796@users.noreply.github.com>
2022-03-24 21:59:29 +0530

    Add files via upload

---
+++ productsearch/src/package-productsearch/ProductRating.java
@@ -0,0 +1,49 @@
+/**@ClassName-ProductRating
+ *@Method-All the getter and setter for the constraints
+ *@Objective-To set and get the value
+ *@param-string,integer
+ *@ReturnType-int ,string
+ *
+ * 
+ */
+
+package com.sebone.java.searchProduct.data;
+
+/**
+ * @author Aaliya Sheikh
+ *
+ */
+public class ProductRating {
+	private int productRatingId;
+	private int productRating;
+	
+	/**
+	 * @return the productRatingId
+	 */
+	public int getProductRatingId() {
+		return productRatingId;
+	}
+	/**
+	 * @param productRatingId the productRatingId to set
+	 */
+	public void setProductRatingId(int productRatingId) {
+		this.productRatingId = productRatingId;
+	}
+	/**
+	 * @return the productRating
+	 */
+	public int getProductRating() {
+		return productRating;
+	}
+	/**
+	 * @param productRating the productRating to set
+	 */
+	public void setProductRating(int productRating) {
+		this.productRating = productRating;
+	}
+	/**
+	 * @return the productRatingType
+	 */
+	
+	
+}
