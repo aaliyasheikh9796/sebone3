59ee40d029bdfa733ee9d1680c062a041a83b31e
aaliyasheikh9796 <101250776+aaliyasheikh9796@users.noreply.github.com>
2022-03-24 21:58:34 +0530

    Add files via upload

---
+++ productsearch/src/package-productsearch/ProductCategory.java
@@ -0,0 +1,49 @@
+/**@ClassName-ProductCategory
+ *@Method-All the getter and setter for the constraints
+ *@Objective-To set and get the value
+ *@param-string,integer
+ *@ReturnType-int ,string
+ *
+ * 
+ */
+package com.sebone.java.searchProduct.data;
+
+/**
+ * @author Aaliya Sheikh
+ *
+ */
+public class ProductCategory {
+	private int ProductCategoryId;
+	private String ProductCategoryName;
+	
+	/**
+	 * @return the productCategoryId
+	 */
+	public int getProductCategoryId() {
+		return ProductCategoryId;
+	}
+	/**
+	 * @param productCategoryId the productCategoryId to set
+	 */
+	public void setProductCategoryId(int productCategoryId) {
+		ProductCategoryId = productCategoryId;
+	}
+	/**
+	 * @return the productCategoryName
+	 */
+	public String getProductCategoryName() {
+		return ProductCategoryName;
+	}
+	/**
+	 * @param productCategoryName the productCategoryName to set
+	 */
+	public void setProductCategoryName(String productCategoryName) {
+		ProductCategoryName = productCategoryName;
+	}
+	/**
+	 * @return the productDescription
+	 */
+	
+	}
+
+
